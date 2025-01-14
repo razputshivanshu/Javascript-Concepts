faa0d0e5f15684989c69f4ca84c994782c2bcd50
razputshivanshu <razputshivanshu@gmail.com>
2025-01-14 12:56:43 +0530

    Learnt datatypes in js

---
+++ Lecture 1_Datatypes/index.js
@@ -0,0 +1,19 @@
+console.log('hello')
+console.log(1234434243)
+console.log(typeof (10));
+console.log(typeof "abcdefghi")
+console.log(typeof "we'll learn something new in the upcoming videos!!!");
+// If we want to use ' quote in the string then we can use "" in the string and vice versa.
+console.log(typeof 'we"ll learn something new in the upcoming videos!!!');
+var x = 10;
+
+console.log(`Hi!! My name is Shivanshu Chauhan.
+  I am currently studiying in chandigarh University.`);
+
+
+  // console.log("Hi!! My name is Shivanshu Chauhan.
+// I am currently studiying in chandigarh University.");
+  
+// The above statement will give error because it does not support multilines.
+
+
